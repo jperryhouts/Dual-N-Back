27c5de9fc86e11d4a48ff3a87fd150c9967949ac
Jonathan Perry-Houts <6118266+jperryhouts@users.noreply.github.com>
2021-10-01 18:27:01 -0700

    Add manual reset N button, and improve some outdated code

--- js/logic.js
+++ js/logic.js
@@ -16,7 +16,7 @@ if ('serviceWorker' in navigator) {
       reg.onupdatefound = function() {
         // The updatefound event implies that reg.installing is set; see
         // https://w3c.github.io/ServiceWorker/#service-worker-registration-updatefound-event
-        var installingWorker = reg.installing;
+        let installingWorker = reg.installing;
 
         installingWorker.onstatechange = function() {
           switch (installingWorker.state) {
@@ -33,7 +33,7 @@ if ('serviceWorker' in navigator) {
                 sw_msgs = 'Content is cached for offline use';
               }
               try {
-                get_screen().getElementById("msgs").innerHTML = sw_msgs;
+                get_screen().getElementById("msgs").textContent = sw_msgs;
                 sw_msgs = '';
               } catch (e) {}
               break;
@@ -79,6 +79,33 @@ var letter_hits = 0;
 var d_prime = 0;
 var time = 0;
 
+function compare_dates(a, b) {
+    return a.getDate() === b.getDate()
+        && a.getMonth() === b.getMonth()
+        && a.getFullYear() === b.getFullYear();
+}
+
+function is_today(d) {
+    return compare_dates(new Date(d), new Date());
+}
+
+function cloner(e) {
+    let newone = e.cloneNode(true);
+    e.parentNode.replaceChild(newone, e);
+    return newone;
+}
+
+function set_toggle(e, state) {
+    e.getElementsByTagName('rect')[0].setAttribute('fill', (state)   ? '#99ccff' : 'grey');
+    e.getElementsByTagName('circle')[0].setAttribute('fill', (state) ? '#226699' : 'grey');
+    e.getElementsByTagName('circle')[0].setAttribute('cx', (state)   ? '10' : '-10');
+}
+
+function replaceEventListener(e, evt, action) {
+    let elm = cloner(e);
+    elm.addEventListener(evt, action, Modernizr.passiveeventlisteners ? {passive: true}: false);
+}
+
 function get_screen() {
     return document.getElementById('thescreen').contentWindow.document;
 }
@@ -88,35 +115,14 @@ function get_menu() {
 }
 
 function get_n_games() {
-    var today = new Date();
-    var games = stats['games'];
-    var gamecount = 0;
-    for (var i=games.length-1; i>-1; i--) {
-        var date = new Date(games[i]['time']);
-        if (date.getDate() == today.getDate()
-            && date.getMonth() == today.getMonth()
-            && date.getFullYear() == today.getFullYear()) {
+    let games = stats['games'];
+    let gamecount = 0;
+    for (let i=games.length-1; i>-1; i--) {
+        if (is_today(games[i]['time'])) {
             gamecount += 1;
         } else { break; } // assumes stats array is sorted.
     }
     return gamecount;
-}
-
-function set_toggle(e, state) {
-    e.getElementsByTagName('rect')[0].setAttribute('fill', (state)   ? '#99ccff' : 'grey');
-    e.getElementsByTagName('circle')[0].setAttribute('fill', (state) ? '#226699' : 'grey');
-    e.getElementsByTagName('circle')[0].setAttribute('cx', (state)   ? '10' : '-10');
-}
-
-function cloner(e) {
-    var newone = e.cloneNode(true);
-    e.parentNode.replaceChild(newone, e);
-    return newone;
-}
-
-function replaceEventListener(e, evt, action) {
-    var elm = cloner(e);
-    elm.addEventListener(evt, action, Modernizr.passiveeventlisteners ? {passive: true}: false);
 }
 
 function init_home() {
@@ -124,9 +130,9 @@ function init_home() {
     replaceEventListener(get_screen().getElementById("#gear"), clickEvnt, function(e)  { window.history.pushState({'page':'config'}, '', '');  goto_config();});
     replaceEventListener(get_screen().getElementById("#help"), clickEvnt, function(e)  { window.history.pushState({'page':'help'},   '', '');  goto_help();});
     replaceEventListener(get_screen().getElementById("#graph"), clickEvnt, function(e) { window.history.pushState({'page':'stats'},  '', '');  goto_stats();});
-    document.getElementById("title").innerHTML = 'N = '+N;
-    document.getElementById("ngames").innerHTML = get_n_games() + ' / 20 Today';
-    get_screen().getElementById("msgs").innerHTML = sw_msgs;
+    document.getElementById("title").textContent = `N = ${N}`;
+    document.getElementById("ngames").textContent = `${get_n_games()} / 20 Today`;
+    get_screen().getElementById("msgs").textContent = sw_msgs;
     sw_msgs = '';
 }
 
@@ -159,7 +165,7 @@ function goto_game(callback) {
     document.getElementById('#play').style.display = 'none';
     document.getElementById('thescreen').contentWindow.location.replace('/screens/game.html');
     document.getElementById('thescreen').onload = function (e) {
-        get_screen().getElementById("title").innerHTML = 'N = '+N;
+        get_screen().getElementById("title").textContent = `N = ${N}`;
         replaceEventListener(get_screen().getElementById("vis_button"), clickEvnt, function(e) {  vis_click();});
         replaceEventListener(get_screen().getElementById("letter_button"), clickEvnt, function(e) { letter_click();});
         replaceEventListener(get_screen().getElementById("#back"), clickEvnt, function(e) {
@@ -179,12 +185,12 @@ function goto_score() {
         replaceEventListener(get_screen().getElementById("#back"), clickEvnt, function(e) {  window.history.back(); });
         replaceEventListener(get_screen().getElementById("#play"), clickEvnt, function(e) {  start_game(true); });
 
-        get_screen().getElementById("vis_hits").innerHTML = vis_hits;
-        get_screen().getElementById("vis_misses").innerHTML = vis_misses;
-        get_screen().getElementById("vis_wrong").innerHTML = vis_wrong;
-        get_screen().getElementById("letter_hits").innerHTML = letter_hits;
-        get_screen().getElementById("letter_misses").innerHTML = letter_misses;
-        get_screen().getElementById("letter_wrong").innerHTML = letter_wrong;
+        get_screen().getElementById("vis_hits").textContent      = ""+vis_hits;
+        get_screen().getElementById("vis_misses").textContent    = ""+vis_misses;
+        get_screen().getElementById("vis_wrong").textContent     = ""+vis_wrong;
+        get_screen().getElementById("letter_hits").textContent   = ""+letter_hits;
+        get_screen().getElementById("letter_misses").textContent = ""+letter_misses;
+        get_screen().getElementById("letter_wrong").textContent  = ""+letter_wrong;
 
         if (d_prime > 0.85) {
             get_screen().getElementById("title").style.color = 'green';
@@ -197,12 +203,14 @@ function goto_score() {
             get_screen().getElementById("level").style.fill = 'black';
         }
 
-        get_screen().getElementById("title").innerHTML = "d' = "+Math.round(d_prime*100)+'%';
-        get_screen().getElementById("level").innerHTML = "N = "+N;
-        get_screen().getElementById("ngames").innerHTML = get_n_games() + ' / 20 Today';
+        get_screen().getElementById("title").textContent = `d' = ${Math.round(d_prime*100)}%`;
+        get_screen().getElementById("level").textContent = `N = ${N}`;
+        get_screen().getElementById("ngames").textContent = `${get_n_games()} / 20 Today`;
 
+        let twitter_text = encodeURIComponent(`I made it to N=${N}!`);
+        let twitter_link = encodeURIComponent('https://dual-n-back.io/');
         get_screen().getElementById('twitter_share').setAttribute('href',
-            'https://twitter.com/intent/tweet/?text='+encodeURIComponent('I made it to N='+N+'! ')+'&url='+encodeURIComponent('https://dual-n-back.io/'));
+            `https://twitter.com/intent/tweet/?text=${twitter_text}&url=${twitter_link}`);
     }
 }
 
@@ -218,11 +226,12 @@ function goto_stats() {
 function goto_config() {
     document.getElementById('themenu').contentWindow.location.replace('/screens/config.html');
     document.getElementById('themenu').onload = function (e) {
-        replaceEventListener(get_menu().getElementById("#download_stats"), clickEvnt, function(e) {  download_stats();});
-        replaceEventListener(get_menu().getElementById("#clear_storage"), clickEvnt, function(e) {  clear_storage_btnclk();});
-        replaceEventListener(get_menu().getElementById("#upload_stats"), "change", function(e) {  upload_cfg(e);});
-        replaceEventListener(get_menu().getElementById("#back"), clickEvnt, function(e) {  window.history.back(); });
-        replaceEventListener(get_menu().getElementById("reset_n"), clickEvnt, function(e) {       toggle_reset_n(); });
+        replaceEventListener(get_menu().getElementById("#download_stats"), clickEvnt,   download_stats);
+        replaceEventListener(get_menu().getElementById("#do_n_reset"), clickEvnt,       reset_n);
+        replaceEventListener(get_menu().getElementById("#clear_storage"), clickEvnt,    clear_storage_btnclk);
+        replaceEventListener(get_menu().getElementById("#upload_stats"), "change",      upload_cfg);
+        replaceEventListener(get_menu().getElementById("#back"), clickEvnt,             go_back);
+        replaceEventListener(get_menu().getElementById("reset_n"), clickEvnt,           toggle_reset_n);
         set_toggle(get_menu().getElementById("reset_n"), cfg["reset_n"]);
         show_menu();
     }
@@ -244,14 +253,23 @@ function toggle_reset_n() {
     localStorage.setItem('config', JSON.stringify(cfg));
 }
 
+function reset_n() {
+    N = 1;
+    localStorage.setItem("N", 1);
+}
+
+function go_back() {
+    window.history.back();
+}
+
 function download_stats() {
-    var elm = cloner(get_menu().getElementById('#download_stats'));
+    let elm = cloner(get_menu().getElementById('#download_stats'));
     elm.style.webkitAnimationPlayState = 'running';
     elm.style.animationPlayState = 'running';
 
-    var backups = { "N": N, "stats": stats };
-    var blob = new Blob([JSON.stringify(backups)], {type: "text"});
-    var element = get_screen().createElement('a');
+    let backups = { "N": N, "stats": stats };
+    let blob = new Blob([JSON.stringify(backups)], {type: "text"});
+    let element = get_screen().createElement('a');
     element.setAttribute('href', URL.createObjectURL(blob));
     element.setAttribute('download', "N-Back_Stats"+(new Date()).toJSON()+".json");
     element.style.display = 'none';
@@ -262,11 +280,11 @@ function download_stats() {
 
 function upload_cfg(event) {
     if (window.File && window.FileReader && window.FileList && window.Blob) {
-        var f = event.target.files[0];
+        let f = event.target.files[0];
         if (f) {
-            var r = new FileReader();
+            let r = new FileReader();
             r.addEventListener("load", function(event) {
-                var asjson = JSON.parse(event.target.result);
+                let asjson = JSON.parse(event.target.result);
                 stats = asjson["stats"];
                 N = asjson["N"];
                 localStorage.setItem("stats", JSON.stringify(stats));
@@ -291,7 +309,7 @@ function do_clear_storage() {
 
 function clear_storage_btnclk() {
     if (confirm('Really clear all app data?')) {
-        var elm = cloner(get_menu().getElementById('#clear_storage'));
+        let elm = cloner(get_menu().getElementById('#clear_storage'));
         elm.style.webkitAnimationPlayState = 'running';
         elm.style.animationPlayState = 'running';
         do_clear_storage();
@@ -300,35 +318,35 @@ function clear_storage_btnclk() {
 
 function set_active(box) {
     if (box >= 0 && box < 8) {
-        var elm = cloner(get_screen().getElementsByClassName('box')[box]);
+        let elm = cloner(get_screen().getElementsByClassName('box')[box]);
         elm.style.webkitAnimationPlayState = 'running';
         elm.style.animationPlayState = 'running';
     }
 }
 
 function vis_click() {
-    var delay = (new Date()).valueOf() - timestep_start;
+    let delay = Date.now() - timestep_start;
     vis_delays.push(delay);
     vis_clicks.push(time-1);
     get_screen().getElementById('vis_button').style.backgroundColor = '#609f9f';
 }
 
 function letter_click() {
-    var delay = (new Date()).valueOf() - timestep_start;
+    let delay = Date.now() - timestep_start;
     letter_delays.push(delay);
     letter_clicks.push(time-1);
     get_screen().getElementById('letter_button').style.backgroundColor = '#609f9f';
 }
 
 function is_in(a, b) {
-    for (var i=0; i<b.length; i++)
+    for (let i=0; i<b.length; i++)
         if (a == b[i])
             return true;
     return false;
 }
 
 function update_stats() {
-    var entry = { "time": (new Date()).toJSON(), "N": N,
+    let entry = { "time": (new Date()).toJSON(), "N": N,
         "vStack": vis_stack, "vClicks": vis_clicks, "vDelays": vis_delays,
         "lStack": letter_stack, "lClicks": letter_clicks, "lDelays": letter_delays,
         "v": 1.0 };
@@ -343,7 +361,7 @@ function calc_score() {
     letter_misses = 0;
     vis_hits = 0;
     letter_hits = 0;
-    for (var i=N; i<vis_stack.length; i++) {
+    for (let i=N; i<vis_stack.length; i++) {
         if (vis_stack[i] == vis_stack[i-N]) {
             if (vis_clicks.indexOf(i) > -1) {
                 vis_hits += 1;
@@ -368,8 +386,8 @@ function calc_score() {
         }
     }
 
-    var hit_rate = (vis_hits/6.0 + letter_hits/6.0)/2.0;
-    var false_alarm_rate = (vis_wrong/(vis_stack.length-6) + letter_wrong/(vis_stack.length - 6))/2.0;
+    let hit_rate = (vis_hits/6.0 + letter_hits/6.0)/2.0;
+    let false_alarm_rate = (vis_wrong/(vis_stack.length-6) + letter_wrong/(vis_stack.length - 6))/2.0;
     d_prime = hit_rate - false_alarm_rate;
 
     if (d_prime > 0.85) {
@@ -384,12 +402,12 @@ function calc_score() {
     }
 }
 
-function timestep() {
+function do_timestep() {
     get_screen().getElementById('vis_button').style.backgroundColor = '#d9d9d9';
     get_screen().getElementById('letter_button').style.backgroundColor = '#d9d9d9';
     if (time < vis_stack.length) {
         set_active(vis_stack[time]);
-        timestep_start = (new Date()).valueOf();
+        timestep_start = Date.now();
         snds[letter_stack[time]].currentTime = 0;
         snds[letter_stack[time]].play();
         time += 1;
@@ -406,10 +424,10 @@ function timestep() {
 }
 
 function build_stacks() {
-    var next = -1;
+    let next = -1;
 
-    var visual_matches = [];
-    var auditory_matches = [];
+    let visual_matches = [];
+    let auditory_matches = [];
     // Choose four random timesteps in which there will be a
     // visual match
     while (visual_matches.length < 4) {
@@ -434,8 +452,8 @@ function build_stacks() {
         }
     }
 
-    var visual_stack = [];
-    var auditory_stack = [];
+    let visual_stack = [];
+    let auditory_stack = [];
     // Randomly assign first N rounds
     while (visual_stack.length < N) {
         next = Math.floor(Math.random()*(8));
@@ -478,24 +496,24 @@ function build_stacks() {
 
 function load_snds() {
     if (snds.length == 0) {
-        var clips = ['/audio/B', '/audio/C', '/audio/D', '/audio/G', '/audio/H',
+        let clips = ['/audio/B', '/audio/C', '/audio/D', '/audio/G', '/audio/H',
             '/audio/K', '/audio/P', '/audio/Q', '/audio/T', '/audio/W'];
 
         if (document.createElement('audio').canPlayType('audio/ogg'))
-            for (var i=0; i<clips.length; i++) {
-                var clip = document.createElement('audio');
+            for (let i=0; i<clips.length; i++) {
+                let clip = document.createElement('audio');
                 clip.setAttribute('src',  clips[i]+'.ogg');
                 snds.push(clip);
             }
         else if (document.createElement('audio').canPlayType('audio/mpeg'))
-            for (var i=0; i<clips.length; i++) {
-                var clip = document.createElement('audio');
+            for (let i=0; i<clips.length; i++) {
+                let clip = document.createElement('audio');
                 clip.setAttribute('src',  clips[i]+'.mp3');
                 snds.push(clip);
             }
         else
-            for (var i=0; i<clips.length; i++) {
-                var clip = document.createElement('audio');
+            for (let i=0; i<clips.length; i++) {
+                let clip = document.createElement('audio');
                 clip.setAttribute('src',  clips[i]+'.wav');
                 snds.push(clip);
             }
@@ -510,12 +528,12 @@ function start_game(isRestart) {
         window.history.pushState({'page':'game'}, '', '');
 
     //load_snds();
-    for (var i=0; i<snds.length; i++) {
+    for (let i=0; i<snds.length; i++) {
         snds[i].load();
         snds[i].volume = 0.65;
     }
 
-    var stacks = build_stacks(N);
+    let stacks = build_stacks(N);
     vis_stack = stacks[0];
     letter_stack = stacks[1];
 
@@ -526,7 +544,7 @@ function start_game(isRestart) {
     time = 0;
     goto_game(function() {
         // Start game
-        myInterval = setInterval( "timestep()", iFrequency );  // run
+        myInterval = setInterval(do_timestep, iFrequency);  // run
     });
 }
 
@@ -542,19 +560,18 @@ window.onpopstate = function(event) {
         goto_stats();
 };
 
-window.onload = function() {
+window.addEventListener("load", function() {
     try { stats = JSON.parse(localStorage.getItem("stats")); } catch (err)  { }
     try { N = parseInt(localStorage.getItem("N")); }           catch (err)  { }
-    try { cfg = JSON.parse(localStorage.getItem('config')); }  catch (err)  { }
+    try { cfg = JSON.parse(localStorage.getItem("config")); }  catch (err)  { }
 
     if (!stats) stats = { 'games': [] };
     if (!N)     N = 1;
     if (!cfg)   cfg = { 'reset_n': false };
 
-    var today = new Date();
-    if (stats['games'].length > 0 && cfg['reset_n']) {
-        var date = new Date(stats['games'][stats['games'].length-1]['time']);
-        if (date.getDate() != today.getDate() || date.getMonth() != today.getMonth() || date.getFullYear() != today.getFullYear()) {
+    if (stats['games'].length > 0) {
+        let latest_game = stats['games'][stats['games'].length-1];
+        if (cfg['reset_n'] && !is_today(latest_game['time'])) {
             N = 1;
         }
     }
@@ -562,4 +579,4 @@ window.onload = function() {
     window.history.pushState({'page':'home'}, '', '');
     goto_home();
     load_snds();
-};
+});
